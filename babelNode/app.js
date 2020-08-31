dfca44cf7fcf1e975c6bcf7fde70f8fbf9754711
MagniezJ <67460476+MagniezJ@users.noreply.github.com>
2020-08-31 11:10:49 +0200

    1.2.2

--- babelNode/app.js
+++ babelNode/app.js
@@ -5,40 +5,71 @@ const bodyParser = require('body-parser');
 const app = express();
 //On instancie le port //
 const port = 3030;
-const dbname='main.db';
+const dbname = 'main.db';
+let user = {
+    isAdmin: false,
+};
 //ouverture bd
 var livereload = require("livereload");
 var server = livereload.createServer();
 server.watch(__dirname + "/public");
-let db=new sqlite3.Database(dbname, err=>{
-    if(err)
+
+let db = new sqlite3.Database(dbname, err => {
+    if (err)
         throw err
-    console.log('Database stated on '+ dbname)
+    console.log('Database stated on ' + dbname)
 });
 
 
 app.set('view engine', 'ejs'); /* prise en compte dossier ejs */
 //Déclarer des middleware -check tuto//
 app.use(express.static("public")); /* prise en compte du dossier public ( page static */
-app.use(bodyParser.urlencoded({extended: false
+app.use(bodyParser.urlencoded({
+    extended: false
 })); /* recuperation des données du body */
 
 app.get("/", (req, res) => {
-    res.render('index'); 
-/*  redirection d'accueil*/
+    res.render('index', {
+        admin: user.isAdmin
+    });
+    /*  redirection d'accueil*/
 })
 
+
+app.post('/', (req, res) => {
+    console.log(user.isAdmin);
+    let mdp = req.body.pass;
+    const fo = '((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{6,}';
+    let p = 0;
+    
+    if (mdp.value.match(fo) == true) {
+        p = 1;
+    } else {
+        p = 0;
+    }
+    if (p == 0) {
+        user.isAdmin = false;
+        console.log("retry")
+    } else {
+        user.isAdmin = true;
+        console.log("cool");
+    }
+    res.render('index', {
+        admin: user.isAdmin
+    });
+
+});
+
 app.get('/new', (req, res) => {
-    res.render('new'); 
-/*  redirection d'accueil*/
+    res.render('new');
+    /*  redirection d'accueil*/
 })
-db.close(err=>{
-    if(err)
-    throw err
-console.log('Database close')
+db.close(err => {
+    if (err)
+        throw err
+    console.log('Database close')
 })
 //On ecoute le port//
 app.listen(port, function () {
     console.log("");
 });
-
